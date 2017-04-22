8c78845cbd76bb97ab15cbde6849205a295692e1
Bill Proudfoot <user@CODECLAN031.local>
2017-04-22 15:35:05 +0100

    Added priority and category art to ToDo java

--- app/src/main/java/example/codeclan/com/todolist/ToDo.java
+++ app/src/main/java/example/codeclan/com/todolist/ToDo.java
@@ -6,14 +6,20 @@ package example.codeclan.com.todolist;
 
 public class ToDo {
 
+    private int priority;
     private String todo;
     private String detail;
+    private int image;
 
-    public ToDo( String todo, String detail){
+    public ToDo( Integer priority, String todo, String detail, int image){
+        this.priority = priority;
         this.todo = todo;
         this.detail = detail;
+        this.image =image;
     }
 
+    public Integer getPriority(){return priority;}
     public String getTodo() {return todo;}
     public String getDetail() {return detail;}
+    public int getImage(){return image;}
 }
