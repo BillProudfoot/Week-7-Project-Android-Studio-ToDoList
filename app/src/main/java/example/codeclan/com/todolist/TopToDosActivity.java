fa61af6cd420b252db855e471d6f337656ee9dbc
Bill Proudfoot <user@CODECLAN031.local>
2017-04-22 21:03:20 +0100

    base layout

--- app/src/main/java/example/codeclan/com/todolist/TopToDosActivity.java
+++ app/src/main/java/example/codeclan/com/todolist/TopToDosActivity.java
@@ -8,28 +8,26 @@ import android.widget.ListView;
 
 import java.util.ArrayList;
 
-import static android.R.attr.id;
-
-public class ToDosActivity extends AppCompatActivity {
+public class TopToDosActivity extends AppCompatActivity {
     ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(layout.todos_list);
+        setContentView(R.layout.todos_list);
 
         TopToDos topToDos = new TopToDos();
         ArrayList<ToDo> list = topToDos.getList();
 
         TopToDosAdapter toDosAdapter = new TopToDosAdapter(this, list);
 
-        ListView listView = (ListView) findViewById(id.list);
+        ListView listView = (ListView) findViewById(R.id.list);
         listView.setAdapter(toDosAdapter);
     }
 
     public void getToDo(View listItem) {
         ToDo toDo = (ToDo) listItem.getTag();
-        Log.d(" To Do : ", toDo.gettoDo());
+        Log.d(" To Do : ", toDo.getTodo());
     }
 }
--- app/src/main/java/example/codeclan/com/todolist/TopToDosAdapter.java
+++ app/src/main/java/example/codeclan/com/todolist/TopToDosAdapter.java
@@ -5,33 +5,42 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * Created by user on 22/04/2017.
  */
 
-public class TopToDosAdapter extends ArrayAdapter<ToDo> {
+public class TopToDosAdapter extends ArrayAdapter<ToDo>{
 
 
-    public class TopToDosAdapter(Context context, ArrayList<ToDo> toDos) {
-        super(context, 0, toDos);
+    public TopToDosAdapter(Context context, ArrayList<ToDo> todos){
+        super(context, 0, todos);
     }
 
     @Override
     public View getView(int position, View listItemView, ViewGroup parent){
 
         if (listItemView == null){
-            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.toDos_item, parent, false);
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.todos_item, parent, false);
         }
 
         ToDo currentToDo = getItem(position);
+
+        TextView priority = (TextView) listItemView.findViewById(R.id.priority);
+        priority.setText(currentToDo.getPriority().toString());
 
         TextView todo = (TextView) listItemView.findViewById(R.id.todo);
         todo.setText(currentToDo.getTodo().toString());
 
         TextView detail = (TextView) listItemView.findViewById(R.id.detail);
         detail.setText(currentToDo.getDetail().toString());
+
+        ImageView myImageView = (ImageView) listItemView.findViewById(R.id.category_art);
+        myImageView.setImageResource(currentToDo.getImage());
 
         listItemView.setTag(currentToDo);
 
