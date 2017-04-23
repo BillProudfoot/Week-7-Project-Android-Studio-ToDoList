093fd9abc87128549dfb62ce2908ff1cca8d158c
Bill Proudfoot <user@CODECLAN031.local>
2017-04-23 12:15:18 +0100

    having issues with onClickable

--- app/src/main/java/example/codeclan/com/todolist/TopToDosActivity.java
+++ app/src/main/java/example/codeclan/com/todolist/TopToDosActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -17,14 +19,32 @@ public class TopToDosActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.todos_list);
 
+        listView = (ListView)findViewById(R.id.list);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,setUpStringsList());
+
         TopToDos topToDos = new TopToDos();
         ArrayList<ToDo> list = topToDos.getList();
 
         TopToDosAdapter toDosAdapter = new TopToDosAdapter(this, list);
 
-        ListView listView = (ListView) findViewById(R.id.list);
+
+
         listView.setAdapter(toDosAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+               String selected = (String)listView.getItemAtPosition(position);
+                Log.d("ListView:", selected + "selected");
+            }
+        });
     }
+
+    private ArrayList<String> setUpStringsList(){
+
+    }
+
 
     public void getToDo(View listItem) {
         ToDo toDo = (ToDo) listItem.getTag();
