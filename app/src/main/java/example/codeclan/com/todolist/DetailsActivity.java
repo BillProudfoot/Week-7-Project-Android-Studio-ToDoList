530646c4d67670e830ffe4e1514141b4dc9e21c9
Bill Proudfoot <user@CODECLAN031.local>
2017-04-25 11:26:51 +0100

    check box added to detail page but not saving yet

--- app/src/main/java/example/codeclan/com/todolist/DetailsActivity.java
+++ app/src/main/java/example/codeclan/com/todolist/DetailsActivity.java
@@ -1,54 +1,71 @@
 package example.codeclan.com.todolist;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
-
-import java.util.ArrayList;
 
 /**
  * Created by user on 24/04/2017.
  */
 
 public class DetailsActivity extends AppCompatActivity {
+    private CheckBox check1;
+    private Button button_done_sel;
     ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.details_list);
+        addListenerOnButton();
 
-        ArrayList<Task> list = SavedTextPreferences.getTasks(this);
-
-        TopTasksAdapter tasksAdapter = new TopTasksAdapter(this, list);
-
-        final ListView listView = (ListView) findViewById(R.id.list);
-        listView.setAdapter(tasksAdapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id){
-                String selected = (String)listView.getItemAtPosition(position);
-            }
-        });
-
-    public void getTask(View listItem) {
-        Task task = (Task) listItem.getTag();
+        String taskAsString = getIntent().getExtras().getString("task");
 
         Gson gson = new Gson();
 
-        String detailAsString = gson.toJson(task);
+        Task task = gson.fromJson(taskAsString, Task.class);
 
-        Intent intent = new Intent(this, TasksActivity.class);
-        intent.putExtra("detail", detailAsString);
+        TextView textView = (TextView) findViewById(R.id.detail);
 
-        startActivity(intent);
+        textView.setText(task.getDetail());
+//        ArrayList<Task> list = SavedTextPreferences.getTasks(this);
+//
+//        TopTasksAdapter tasksAdapter = new TopTasksAdapter(this, list);
+//
+//        final ListView listView = (ListView) findViewById(R.id.list);
+//        listView.setAdapter(tasksAdapter);
+//
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                String selected = (String) listView.getItemAtPosition(position);
+//            }
+//        });
     }
+
+    public void addListenerOnButton(){
+        check1 = (CheckBox)findViewById(R.id.checkbox_done);
+        button_done_sel =(Button)findViewById(R.id.button);
+
+        button_done_sel.setOnClickListener(
+                new View.OnClickListener(){
+                    @Override
+                    public void onClick(View view){
+                        StringBuffer result = new StringBuffer();
+                        result.append("Task : ").append(check1.isChecked());
+
+                        Toast.makeText(DetailsActivity.this,result.toString(),Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
+    }
+
 
 }
--- app/src/main/java/example/codeclan/com/todolist/TopTasksActivity.java
+++ app/src/main/java/example/codeclan/com/todolist/TopTasksActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.google.gson.Gson;
@@ -28,13 +27,13 @@ public class TopTasksActivity extends AppCompatActivity {
         final ListView listView = (ListView) findViewById(R.id.list);
         listView.setAdapter(tasksAdapter);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id){
-               String selected = (String)listView.getItemAtPosition(position);
-                Log.d("ListView:", selected + "selected");
-            }
-        });
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+//               String selected = (String)listView.getItemAtPosition(position);
+//                Log.d("ListView:", selected + "selected");
+//            }
+//        });
     }
 
 //    private ArrayList<String> setUpStringsList(){
@@ -51,7 +50,7 @@ public class TopTasksActivity extends AppCompatActivity {
 
         String taskAsString = gson.toJson(task);
 
-        Intent intent = new Intent(this, TasksActivity.class); // UPDATED
+        Intent intent = new Intent(this, DetailsActivity.class); // UPDATED
         intent.putExtra("task", taskAsString); // UPDATED
 
         startActivity(intent); // UPDATED
